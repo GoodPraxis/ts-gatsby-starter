b97415025c34236676e00dbd41d75dfd51d2a132
Maciek Baron <maciej@goodpraxis.uk>
2023-01-25 11:35:04 +0000

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: 'Clarissa Frontend',
+    title: 'Website',
     siteUrl: 'https://www.yourdomain.tld',
   },
   // More easily incorporate content into your pages through automatic
